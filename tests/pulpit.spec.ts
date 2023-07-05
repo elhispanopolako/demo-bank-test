d087ad40714660e7842fda03509d7ce7af2193d9
Tadeusz Woźniak <twozniak@consdata.com>
2023-07-05 21:07:00 +0200

    new pulpit test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -13,6 +13,18 @@ test.describe('Pulpit test', () => {
         await page.getByTestId('close-button').click();
         await expect(page.getByRole('link', { name: 'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza' })).toBeVisible()
         await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza')
+    })
+    test('Successfull phone top-up from the dashboard', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/');
+        await page.getByTestId('login-input').fill('ltesterl');
+        await page.getByTestId('password-input').fill('password');
+        await page.getByTestId('login-button').click();
 
+        await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+        await page.locator('#widget_1_topup_amount').fill('2121');
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+        await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 2121,00PLN na numer 500 xxx xxx');
     })
 })
